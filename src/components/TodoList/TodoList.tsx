035d44de753bbc8e47045c747fc7589fc5458135
Crysis <spidrunner2008@gmail.com>
2024-07-16 09:25:58 +0300

    created component TodoList

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import styles from "./TodoList.module.css";
+import cfg from "../../CTLAConfig";
+import TodoItem from "../TodoItem/TodoItem";
+
+const TodoList = () => {
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+  useEffect(() => {
+    fetch(cfg.BACKEND_GET_DATA_URL + "/todos.json").then((res) =>
+      res.json().then((data) => setTodos(data))
+    );
+  });
+  function todosCreater(todos: TodoItem[]) {
+    return todos.map((todo) => <TodoItem todo={todo} />);
+  }
+  return <div className={styles.wrapper}>{todosCreater(todos)}</div>;
+};
+
+export default TodoList;
