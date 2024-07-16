68ad7f6ebb1c772ee3da0c52da29dd35c55195d5
Crysis <spidrunner2008@gmail.com>
2024-07-16 08:34:09 +0300

    added CTLA config file

---
+++ public/CTLAConfig.ts
@@ -0,0 +1,5 @@
+export const cfg = {
+    BACKEND_URL: 'http://localhost:3000',
+    BACKEND_GET_DATA_URL: 'http://localhost:3000/api/get',
+    BACKEND_POST_DATA_URL: 'http://localhost:3000/api/post',
+}
