aa6f6c4fa1cf6dce3440d7d1aa3e94d76d1ab108
Crysis <spidrunner2008@gmail.com>
2024-07-16 13:38:26 +0300

    writed base logic to get data

--- back/index.js
+++ back/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
 const app = express();
 const PORT = 3624;
 
-app.get("api/get", (req, res) => {
-    res.end("Hello World");
-});
+app.use(cors()); 
+
+app.use('/api/get/todos', express.static(__dirname + '/data/todos.json'));
+
 app.listen(PORT, () => {
     console.log("Server is started!");
     console.log('http://' + "localhost" + `:${PORT}`);
 });
+
