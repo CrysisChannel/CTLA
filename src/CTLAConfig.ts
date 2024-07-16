838b83a3264b304cd6e12b26dfa8fd3a89f28f28
Crysis <spidrunner2008@gmail.com>
2024-07-16 09:24:49 +0300

    moved CTLAConfig file

--- src/CTLAConfig.ts
+++ src/CTLAConfig.ts
@@ -1,5 +1,6 @@
-export const cfg = {
+const cfg = {
     BACKEND_URL: 'http://localhost:3000',
     BACKEND_GET_DATA_URL: 'http://localhost:3000/api/get',
     BACKEND_POST_DATA_URL: 'http://localhost:3000/api/post',
 }
+export default cfg;
