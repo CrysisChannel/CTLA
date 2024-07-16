0746fb6097d13fcaec0069dcb04c93a7baab464d
Crysis <spidrunner2008@gmail.com>
2024-07-16 14:31:19 +0300

    styled component TodoList

--- front/src/components/TodoItem/TodoItem.tsx
+++ front/src/components/TodoItem/TodoItem.tsx
@@ -11,7 +11,11 @@ type Props = {
 const TodoItem = ({ todo }: Props) => {
   return (
     <div key={todo.id} id={"todo-" + todo.id} className={styles.todo}>
-      <input type="checkbox" checked={todo.isCompleted} />
+      <input
+        type="checkbox"
+        checked={todo.isCompleted}
+        className={styles.checkbox}
+      />
       <p>{todo.content}</p>
     </div>
   );
