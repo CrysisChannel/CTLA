f32961e54e5f9b7a035d99725753c03425742ac4
Crysis <spidrunner2008@gmail.com>
2024-07-16 13:51:36 +0300

    writed base logic to post

--- back/index.js
+++ back/index.js
@@ -8,6 +8,29 @@ app.use(cors());
 
 app.use('/api/get/todos', express.static(__dirname + '/data/todos.json'));
 
+app.post("/api/post/todos", (req, res) => {
+    if(!req.readable) res.sendStatus(400);
+    let info = "";
+    req.on('data', (data) => {
+        info += data;
+    });
+    req.on('end', () => {
+        fs.readFile(__dirname + `/data/todos.json`, (error, todos) => {
+            if(error) {
+                res.sendStatus(500);
+                res.end();
+            }
+            let newInfo = todos.toString() ? JSON.parse(todos.toString()) : [];
+            info = JSON.parse(info);
+            if(info.content && info.isCompleted){
+                newInfo.push(info);
+            }
+            fs.writeFile(__dirname + `/data/todos.json`, JSON.stringify(newInfo), (error) => {console.log(error)});  // выводим считанные данные
+            res.end();
+        });
+    });
+}); 
+
 app.listen(PORT, () => {
     console.log("Server is started!");
     console.log('http://' + "localhost" + `:${PORT}`);
