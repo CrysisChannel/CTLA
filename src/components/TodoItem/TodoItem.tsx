0adff82ff9c88af4be7c64ce997182680211ea8d
Crysis <spidrunner2008@gmail.com>
2024-07-16 09:23:09 +0300

    created - component TodoItem

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,20 @@
+import styles from "./TodoItem.module.css";
+
+type TodoItem = {
+  id: number;
+  isCompleted: boolean;
+  content: string;
+};
+type Props = {
+  todo: TodoItem;
+};
+const TodoItem = ({ todo }: Props) => {
+  return (
+    <div key={todo.id} id={"todo-" + todo.id} className={styles.todo}>
+      <input type="checkbox" checked={todo.isCompleted} />
+      <p>{todo.content}</p>
+    </div>
+  );
+};
+
+export default TodoItem;
