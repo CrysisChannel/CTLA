125611deaba0aceee493e6ec85a99df5460ed7f3
Crysis <spidrunner2008@gmail.com>
2024-07-17 05:56:21 +0300

    added logic to change todo status in front

--- front/src/components/TodoItem/TodoItem.tsx
+++ front/src/components/TodoItem/TodoItem.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import cfg from "../../CTLAConfig";
 import styles from "./TodoItem.module.css";
 
 type TodoItem = {
@@ -9,12 +11,26 @@ type Props = {
   todo: TodoItem;
 };
 const TodoItem = ({ todo }: Props) => {
+  const [value, setValue] = useState(todo.isCompleted);
+  function checkboxHandler(e: React.ChangeEvent<HTMLInputElement>) {
+    if (!value) {
+      fetch(cfg.BACKEND_POST_DATA_URL + "/todos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id: todo.id }),
+      });
+    }
+    setValue((state) => !state);
+  }
   return (
     <div key={todo.id} id={"todo-" + todo.id} className={styles.todo}>
       <input
         type="checkbox"
-        checked={todo.isCompleted}
+        checked={value}
         className={styles.checkbox}
+        onChange={checkboxHandler}
       />
       <p>{todo.content}</p>
     </div>
